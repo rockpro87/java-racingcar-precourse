404a72a49f35391efc9daf82b896a20d30872a76
Hyunglok Lee <rockpro87@naver.com>
2022-04-23 00:39:05 +0900

    test: 자동차 위치 테스트 리팩토링

--- src/test/java/racingcar/CarLocationTest.java
+++ src/test/java/racingcar/CarLocationTest.java
@@ -1,29 +1,27 @@
 package racingcar;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarLocationTest {
-    CarLocation carLocation;
+    static CarLocation carLocation;
 
-    @BeforeEach
-    void setup() {
+    @BeforeAll
+    static void setup() {
         carLocation = new CarLocation();
+        carLocation.moveForward();
+        carLocation.moveForward();
     }
 
     @Test
     void 자동차_전진시_위치_숫자_검증() {
-        carLocation.moveForward();
-        carLocation.moveForward();
         assertThat(carLocation.toNumber()).isEqualTo(2);
     }
 
     @Test
     void 자동차_전진시_위치_텍스트_검증() {
-        carLocation.moveForward();
-        carLocation.moveForward();
         assertThat(carLocation.toResultText()).isEqualTo("--");
     }
 }
